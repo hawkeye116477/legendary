1a0d37c35b1f357e9db3090fe3fa9ce06f15041e
hawkeye116477 <hawkeye116477@gmail.com>
2026-05-14 23:40:36 +0200

    Execute through the shell only at Windows

--- legendary/cli.py
+++ legendary/cli.py
@@ -749,7 +749,7 @@ class LegendaryCLI:
             if params.environment:
                 logger.debug('Environment overrides: {}'.format(', '.join(
                     f'{k}={v}' for k, v in params.environment.items())))
-            subprocess.Popen(full_params, cwd=params.working_directory, env=full_env, shell=True)
+            subprocess.Popen(full_params, cwd=params.working_directory, env=full_env, shell=os.name == 'nt')
 
     def _launch_origin(self, args):
         game = self.core.get_game(app_name=args.app_name)
